2e6098eb4288bffd4b414c8898a9d0eb13ae2c4c
P4pyk04L4 <nclscharr@gmail.com>
2024-05-13 17:23:14 +0200

    20240513-1722 - Mise en forme globale

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class SortiesController extends AbstractController
 {
     #[Route('/index', name: 'index')]
-    public function index(): Response
+    public function index(SortieRepository $sortieRepository): Response
     {
+        $sorties = $sortieRepository->findAll();
         return $this->render('sorties/index.html.twig', [
             'controller_name' => 'SortiesController',
+            'sorties' => $sorties
         ]);
     }
 }
